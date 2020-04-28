ba2711bf0f842794fa52bac7a8cc3d7da719d5d9
paulotrujillo <paulo_trujillo@hotmail.com>
2020-04-28 18:20:42 -0500

    Agrega botón a interface
    Agrega un botón con un numero random en como texto.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
 
+//Importar componentes
+//import Elements from './elementos';
+
+
 export function App({ initialData }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>{initialData.appName}</h1>
-      Iniciando el aprendizaje de React, a echarle Atomos!!!!
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
+      Aprendiendo React
       <br />
       <br />
       <button onClick={() => setCount(count + 1)}>{count}</button>
+      <br />
+      <br />
+      <Button />
     </div>
   );
 }
+
+/**
+ * Muestra un boton
+ */
+function Button () {
+  //const [counter, setCounter] = useState(0);
+  return (
+    <button>{Math.random()}</button>
+  ); 
+}
+
+
+
+
+
+
+
 
 
